1d2bf761e694fbbd6ac1d3bdb946be30df0b9383
arthurmigue6557-maker <arthurmigue6557@gmail.com>
2026-03-11 14:08:37 -0300

    The code of game

---
+++ sw.js
@@ -0,0 +1,58 @@
+// ===========================================
+// Snake Game Service Worker
+// ===========================================
+
+const CACHE_NAME = "snake-game-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+
+});
+
+// Activate
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+
+});
+
+// Fetch (serve cached files offline)
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
