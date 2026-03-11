8c716818da383d425042157f3e2627519e486b36
arthurmigue6557-maker <arthurmigue6557@gmail.com>
2026-03-11 14:33:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,14 +5,13 @@
 const CACHE_NAME = "snake-game-v2";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.min.html",
+  "/",
+  "/index.min.html",
   "/favicon.ico"
 ];
 
-// Install Service Worker
+// Install
 self.addEventListener("install", event => {
-
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -20,88 +19,31 @@ self.addEventListener("install", event => {
   );
 
   self.skipWaiting();
-
 });
 
 // Activate
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys.map(key => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
-      );
-    })
+      )
+    )
   );
 
   self.clients.claim();
-
 });
 
-// Fetch (serve cached files offline)
+// Fetch
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
-
 });
-
-});
-
-// Fetch (serve cached files offline)
-self.addEventListener("fetch", event => {
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-
-});
-});
-
-// Fetch (serve cached files offline)
-self.addEventListener("fetch", event => {
-
-  event.respondWith(
-
-    caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request);
-
-      })
-
-  );
-
-});
-
-});
-
-// Fetch (serve cached files offline)
-self.addEventListener("fetch", event => {
-
-  event.respondWith(
-
-    caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request);
-
-      })
-
-  );
-
-});
-
-
 
