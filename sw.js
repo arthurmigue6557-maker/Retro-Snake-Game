3b3336554fd80070eff81cd4c709e7576d762604
arthurmigue6557-maker <arthurmigue6557@gmail.com>
2026-03-11 14:24:53 -0300

    Bug in the code

--- sw.js
+++ sw.js
@@ -46,6 +46,19 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
 
   event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+
+});
+});
+
+// Fetch (serve cached files offline)
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
 
     caches.match(event.request)
       .then(response => {
@@ -75,4 +88,5 @@ self.addEventListener("fetch", event => {
   );
 
 });
+
 
